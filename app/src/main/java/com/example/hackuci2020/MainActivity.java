0b2bcd715d11d0c4141a6288a10ff86bf8b816e3
Naomi <naomibvuong@gmail.com>
2020-02-01 10:47:24 -0800

    Worked on RecyclerView

--- app/src/main/java/com/example/hackuci2020/MainActivity.java
+++ app/src/main/java/com/example/hackuci2020/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -16,13 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        butn = findViewById(R.id.btn);
-        butn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, EventActivity.class));
-            }
-        });
+
     }
 
 }
