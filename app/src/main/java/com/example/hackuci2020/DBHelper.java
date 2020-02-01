fb6c7e8627825a567e5dffc22aad6d2754af6956
burwinliu <burwincollege@gmail.com>
2020-01-31 18:46:09 -0800

    init commit

---
+++ app/src/main/java/com/example/hackuci2020/DBHelper.java
@@ -0,0 +1,24 @@
+package com.example.hackuci2020;
+
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper {
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "FeedReader.db";
+
+    public DBHelper(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
---
+++ app/src/main/java/com/example/hackuci2020/DateTimeInterface.java
@@ -0,0 +1,9 @@
+package com.example.hackuci2020;
+
+import com.example.hackuci2020.DBHelper;
+
+public class DateTimeInterface {
+    public DateTimeInterface(){
+    }
+
+}
