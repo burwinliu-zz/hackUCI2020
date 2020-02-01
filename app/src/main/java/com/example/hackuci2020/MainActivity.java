a6a80f4d3b1cb1e71658b74a5e848d10d258c150
burwinliu <burwincollege@gmail.com>
2020-02-01 10:40:45 -0800

    added a key to work out

--- app/src/main/java/com/example/hackuci2020/MainActivity.java
+++ app/src/main/java/com/example/hackuci2020/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("TESTING_KEY", BuildConfig.bingAPIKey);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
