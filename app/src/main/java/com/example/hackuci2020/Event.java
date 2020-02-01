75376f980088c7a6bc1be982d07de42dfba0de2b
rayfalke <ronnie_school@yahoo.com>
2020-02-01 00:14:28 -0800

    Implemented some of the Event Class

--- app/src/main/java/com/example/hackuci2020/Event.java
+++ app/src/main/java/com/example/hackuci2020/Event.java
@@ -1,4 +1,31 @@
 package com.example.hackuci2020;
 
 public class Event {
+
+    String name;
+    String location;
+    // Location based off of google map widget
+
+    int start_time;
+    // out of 2400. 1200 is 12:00 PM
+    int end_time;
+
+    int day;
+    int month;
+    int year;
+    String description;
+
+    int alert_before;
+    // Alert this many minutes before the event
+
+    public Event(String event_name, String event_location, int start, int end, int d, int m, int y, String event_description){
+        name = event_name;
+        location = event_location;
+        start_time = start;
+        end_time = end;
+        day = d;
+        month = m;
+        year = y;
+        description = event_description;
+    }
 }
