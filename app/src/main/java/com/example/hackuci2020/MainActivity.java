4880b2cee84538c82c4f886b72365005138b3b5e
Naomi <naomibvuong@gmail.com>
2020-02-01 13:39:50 -0800

    Working Main

--- app/src/main/java/com/example/hackuci2020/MainActivity.java
+++ app/src/main/java/com/example/hackuci2020/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.hackuci2020;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,17 +10,46 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
-    Button butn;
+    private static final String TAG = "MainActivity";
+    // vars
+    private ArrayList<String> Names = new ArrayList<>();
+    private ArrayList<String> internetURLs = new ArrayList<>();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.d("TESTING_KEY", BuildConfig.bingAPIKey);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: started.");
+        Button createButton = findViewById(R.id.add_event);
+        createButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, EventActivity.class));
+            }
+        });
+        initImageBitmaps();
+
+    }
+    private void initImageBitmaps() {
+        Log.d(TAG, "initImageBitmaps: preparing bitmaps.");
+        internetURLs.add("shutterstock.com/blog/wp-content/uploads/sites/5/2019/07/Man-Silhouette.jpg");
+        Names.add("Moon");
+        initRecyclerView();
 
 
     }
-
+    private void initRecyclerView() {
+        Log.d(TAG, "initRecyclerView: init recyclerview");
+        RecyclerView recyclerView = findViewById(R.id.recylerview);
+        RecycleViewAdapter adapter = new RecycleViewAdapter(this, Names, internetURLs);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
 }
--- app/src/main/java/com/example/hackuci2020/RecycleViewAdapter.java
+++ app/src/main/java/com/example/hackuci2020/RecycleViewAdapter.java
@@ -1,4 +1,72 @@
 package com.example.hackuci2020;
 
-public class RecycleViewAdapter {
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class RecycleViewAdapter extends RecyclerView.Adapter<RecycleViewAdapter.ViewHolder>{
+    private static final String TAG = "RecycleViewAdapter";
+    private ArrayList<String> ImageNames = new ArrayList<>();
+    private ArrayList<String> Images = new ArrayList<>();
+    private Context mContext;
+
+    public RecycleViewAdapter(Context mcontext, ArrayList<String> imageNames, ArrayList<String> images) {
+        ImageNames = imageNames;
+        Images = images;
+        mContext = mcontext;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_listhours_item, parent, false );
+        ViewHolder holder = new ViewHolder(view);
+
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Log.d(TAG, "onBindViewHolder: called.");
+
+        Glide.with(mContext)
+                .asBitmap()
+                .load(Images.get(position))
+                .into(holder.image);
+        holder.imageName.setText(ImageNames.get(position));
+
+        //on click thing hear to open up either fragment or activity showing the event's details
+    }
+
+    @Override
+    public int getItemCount() {
+        return ImageNames.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        CircleImageView image;
+        TextView imageName;
+        ConstraintLayout parentLayout;
+        public ViewHolder(View itemView) {
+
+            super(itemView);
+            image = itemView.findViewById(R.id.hour_image);
+            imageName = itemView.findViewById(R.id.time);
+            parentLayout = itemView.findViewById(R.id.parent_layout);
+
+        }
+    }
 }
