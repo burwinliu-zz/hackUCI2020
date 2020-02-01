0cf122ae966a09aa10ba737d6c33b25e59d761de
Naomi <naomibvuong@gmail.com>
2020-02-01 09:14:28 -0800

    SDK v-7.28.0.0 and recyclerview

--- app/src/main/java/com/example/hackuci2020/DBHelper.java
+++ app/src/main/java/com/example/hackuci2020/DBHelper.java
@@ -6,6 +6,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DBHelper extends SQLiteOpenHelper {
+
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "FeedReader.db";
 
@@ -21,4 +22,7 @@ public class DBHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
     }
+
+    //public Event getEvent(TimeRepresentation time){
+    //}
 }
