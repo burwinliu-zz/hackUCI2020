0e897f41e6c415b66484918c451f56b2c18989f2
burwinliu <burwincollege@gmail.com>
2020-02-01 19:34:54 -0800

    basic of database finished

--- app/src/main/java/com/example/hackuci2020/APIInterface.java
+++ app/src/main/java/com/example/hackuci2020/APIInterface.java
@@ -9,10 +9,13 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import static java.lang.Math.ceil;
+
 public class APIInterface {
     public APIInterface(){}
 
-    public int getTime(double startLong, double startLat, double endLong, int endLat, int transport) {
+    public int getTime(double startLong, double startLat, double endLong, double endLat, int transport) {
+        // returns time in minutes
         String travelMode, wayPoint1, wayPoint2;
 
         wayPoint1 = startLat + "," + startLong;
@@ -31,7 +34,33 @@ public class APIInterface {
         String page = GetJsonData(url);
         try {
             JSONObject jsonObject = new JSONObject(page);
-            return (int) jsonObject.get("travelDuration");
+            return (int) ceil((int) jsonObject.get("travelDuration")/60);
+        }
+        catch(JSONException e){
+            return -1;
+        }
+    }
+    public int getTime(String start_addr, String end_adr, int transport) {
+        // returns time in minutes
+        String travelMode, wayPoint1, wayPoint2;
+
+        wayPoint1 = start_addr;
+        wayPoint2 = end_adr;
+
+        if (transport == 0)
+            travelMode = "Driving";
+        else
+            travelMode = "Walking";
+
+
+        String url = "http://dev.virtualearth.net/REST/v1/Routes/" + travelMode +
+                "?wp.0=" + wayPoint1 + "&wp.1=" + wayPoint2 +
+                "&key=" + BuildConfig.bingAPIKey;
+
+        String page = GetJsonData(url);
+        try {
+            JSONObject jsonObject = new JSONObject(page);
+            return (int) ceil((int) jsonObject.get("travelDuration")/60);
         }
         catch(JSONException e){
             return -1;
